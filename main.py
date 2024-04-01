5b91b7b664fad8373a97aa83f8fe18dd650638c4
Chamel Nadir Bouacha <lc_bouacha@esi.dz>
2024-04-01 18:02:49 +0100

    fixed the issues of convertion to pdf

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import csv
 from docx import Document
 import os
+from docx2pdf import convert
 
 # Load your list of names from a CSV file
 with open('Participants_List.csv', 'r') as f:
     reader = csv.reader(f)
     names = [row[0] for row in reader]  # adjust this based on the structure of your CSV file
 
-# Load your certificate template
-doc = Document('Template.odt')
-
 for name in names:
-    # Make a copy of the template
-    certificate = doc.copy()
+    # Load your certificate template
+    doc = Document('Template.docx')
 
     # Replace placeholder with the actual name
-    for paragraph in certificate.paragraphs:
-        if 'XYZ' in paragraph.text:
-            paragraph.text = paragraph.text.replace('XYZ', name)
-        if "ABC" in paragraph.text:
-            paragraph.text = paragraph.text.replace("ABC", "Chamel Nadir Bouacha") # Replace ABC With The Ambassador Name
-        if "CSC" in paragraph.text:
-            paragraph.text = paragraph.text.replace("CSC", "Ambassador Challenge Dive Into The World Of Cloud With Azure") # Replace CSC With The Event Name
+    for paragraph in doc.paragraphs:
+        for run in paragraph.runs:
+            if 'XYZ' in run.text:
+                run.text = run.text.replace('XYZ', name)
+            if "ABC" in run.text:
+                run.text = run.text.replace("ABC", "Chamel Nadir Bouacha") # Replace ABC With The Ambassador Name
+            if "CSC" in run.text:
+                run.text = run.text.replace("CSC", "Ambassador Challenge Dive Into The World Of Cloud With Azure") # Replace CSC With The Event Name
+
+    # Create the necessary directories if they don't exist
+    os.makedirs('Output/DOCS/', exist_ok=True)
+    os.makedirs('Output/PDF/', exist_ok=True)
+
     # Save the certificate
-    odt = f'Output/ODT/{name}_certificate.odt'
+    docs = f'Output/DOCS/{name}_certificate.docx'
     pdf = f'Output/PDF/{name}_certificate.pdf'
-    certificate.save(f'Output/ODT/{name}_certificate.odt')
-        # Convert the .odt file to .pdf
-    os.system(f'unoconv -f pdf -o {pdf} {odt}')
+    doc.save(docs)
+
+    # Convert the .docx file to .pdf
+    convert(docs, pdf)
